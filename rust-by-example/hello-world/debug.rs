86a33a8d23afb7c9964c103c38baa3463a84e70a
Peter Norstein <peter.norstein@gmail.com>
2021-07-31 18:38:36 +0200

    Add implementations of traits for structures
    Adding Display implementations for simple structures.

--- rust-by-example/hello-world/debug.rs
+++ rust-by-example/hello-world/debug.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 // Derive the `fmt::Debug` implementation for `Structure`. `Structure`
 // is a structure which contains a single `i32`.
 #[derive(Debug)]
@@ -11,6 +12,33 @@ struct Deep(Structure);
 struct Person<'a> {
     name: &'a str,
     age: u8
+}
+
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+impl fmt::Display for Point2D {
+
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+impl fmt::Display for Structure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
 }
 
 fn main() {
