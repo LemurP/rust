c476c772f0342cae9260cda06a179f8e5a0f5a92
Peter Norstein <peter.norstein@gmail.com>
2021-07-31 12:12:40 +0200

    Remove comment

--- rust-by-example/hello-world/comments.rs
+++ rust-by-example/hello-world/comments.rs
@@ -24,7 +24,7 @@ fn main() {
     // You can manipulate expressions more easily with block comments
     // than with line comments. Try deleting the comment delimiters
     // to change the result:
-    let x = 5 + /* 90 + */ 5;
+    let x = 5 +  90 +  5;
     println!("Is `x` 10 or 100? x = {}", x);
 }
 
